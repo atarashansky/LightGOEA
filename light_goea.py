bd228afc03f21fe5d301cd43abbf7392787e63cd
atarashansky <tarashan@stanford.edu>
2020-02-26 14:52:08 -0800

    Update light_goea.py

--- light_goea.py
+++ light_goea.py
@@ -32,47 +32,69 @@ def GOEA(target_genes,GENE_SETS,goterms=None,fdr_thresh=0.25,p_thresh=1e-3):
         A Pandas DataFrame of enriched GO terms with FDR q values, p values, and associated genes provided.
     """    
     
+    # identify all genes found in `GENE_SETS`
     all_genes = np.unique(np.concatenate(list(GENE_SETS.values())))
     all_genes = np.array(all_genes)
     
+    # if goterms is None, use all the goterms found in `GENE_SETS`
     if goterms is None:
         goterms = np.unique(list(GENE_SETS.keys()))
     else:
         goterms = goterms[np.in1d(goterms,np.unique(list(GENE_SETS.keys())))]
     
+    # ensure that target genes are all present in `all_genes`
     _,ix = np.unique(target_genes,return_index=True)
     target_genes=target_genes[np.sort(ix)]
     target_genes = target_genes[np.in1d(target_genes,all_genes)]
+    
+    # N -- total number of genes
     N = all_genes.size
 
     probs=[]
     probs_genes=[]
     counter=0
-    
+    # for each go term,
     for goterm in goterms:
         if counter%1000==0:
             print(counter)
         counter+=1
+        
+        # identify genes associated with this go term
         gene_set = GENE_SETS[goterm]
+        
+        # B -- number of genes associated with this go term
         B = gene_set.size
+        
+        # b -- number of genes in target associated with this go term
         gene_set_in_target = gene_set[np.in1d(gene_set,target_genes)]
         b = gene_set_in_target.size        
         if b != 0:
+            # calculate the enrichment probability as the cumulative sum of the tail end of a hypergeometric distribution
+            # with parameters (N,B,n,b)
             n = target_genes.size
             num_iter = min(n,B)
             rng = np.arange(b,num_iter+1)
             probs.append(sum([np.exp(log_binomial(n,i)+log_binomial(N-n,B-i) - log_binomial(N,B)) for i in rng]))
         else:
             probs.append(1.0)
+        
+        #append associated genes to a list
         probs_genes.append(gene_set_in_target)
+        
     probs = np.array(probs)    
     probs_genes = np.array(probs_genes)
+    
+    # adjust p value to correct for multiple testing
     fdr_q_probs = probs.size*probs / rankdata(probs,method='ordinal')
+    
+    # filter out go terms based on the FDR q value and p value thresholds
     filt = np.logical_and(fdr_q_probs<fdr_thresh,probs<p_thresh)
     enriched_goterms = goterms[filt]
     p_values = probs[filt]
     fdr_q_probs = fdr_q_probs[filt]    
     probs_genes=probs_genes[filt]
+    
+    # construct the Pandas DataFrame
     gns = []
     for i in probs_genes:
         gns.append(';'.join(i))
@@ -80,5 +102,7 @@ def GOEA(target_genes,GENE_SETS,goterms=None,fdr_thresh=0.25,p_thresh=1e-3):
     enriched_goterms = pd.DataFrame(data=fdr_q_probs,index=enriched_goterms,columns=['fdr_q_value'])
     enriched_goterms['p_value'] = p_values
     enriched_goterms['genes'] = gns
+    
+    # sort in ascending order by the p value
     enriched_goterms = enriched_goterms.sort_values('p_value')   
     return enriched_goterms
